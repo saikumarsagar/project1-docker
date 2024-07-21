930f769b5a5563bf4c460d26162980d65958cc35
saikumarsagar <102918746+saikumarsagar@users.noreply.github.com>
2024-07-21 07:17:48 +0000

    cahng2

--- src/org/foo/ExampleClass.groovy
+++ src/org/foo/ExampleClass.groovy
@@ -1,7 +1,10 @@
+
+// File: my-shared-library/src/org/foo/ExampleClass.groovy
+
 package org.foo
 
 class ExampleClass {
     static void sayHello(String name) {
-        println "Hi, ${name} , what a sudden suppai"
+        println "Hi, ${name}, what's up?"
     }
 }
